2d2f38bc558325274ef42ba3c979f9b4133be13c
Raí Augusto <raiaugusto436@gmail.com>
2025-10-01 12:19:25 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/rai/course/entities/Category.java
+++ src/main/java/com/rai/course/entities/Category.java
@@ -3,12 +3,14 @@ package com.rai.course.entities;
 import java.util.HashSet;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "tb_category")
@@ -19,7 +21,8 @@ public class Category {
     private Long id;
     private String name;
 
-    @Transient
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
     private Set<Product> products = new HashSet<>();
 
     public Category() {
